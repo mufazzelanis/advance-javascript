ef84aad0548c39e96d789437487b595f396ef35f
Mufazzel Hossain <mufazzelanis@gmail.com>
2024-03-05 01:57:02 +0600

    map vs filter continue

---
+++ map-filter.js
@@ -0,0 +1,10 @@
+const numbers = [2, 5 , 6 , 9];
+const output=[];
+for (let i=0; i<numbers.length; i++){
+    const element = numbers[i];
+    const result= element*element;
+    output.push(result);
+}
+console.log(output);
+
+// const numbers=[5,3,6,8,9];
